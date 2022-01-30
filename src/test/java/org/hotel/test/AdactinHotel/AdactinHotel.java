63ea1d9f8d4bb0a81534b728a51a78187927d899
gdinesh2022 <dineshdominates@gmail.com>
2022-01-30 18:43:09 +0530

    Dinesh's Updated Code

--- src/test/java/org/hotel/test/AdactinHotel/AdactinHotel.java
+++ src/test/java/org/hotel/test/AdactinHotel/AdactinHotel.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.Select;
 public class AdactinHotel extends BaseClass {
 	public static void main(String[] args) {
 		
+		//AdactinHotel
 		launchBrowser("Chrome");
 		implicitWait(20);
 		urlLaunch("https://adactinhotelapp.com/index.php");
